4707efae34f3bd08dbc3f39aaa6f227f7049153b
Oluphummie <50441918+Oluphummie@users.noreply.github.com>
2024-11-05 20:30:30 +0100

    Add files via upload

---
+++ Customerdata (1).sql
@@ -0,0 +1,89 @@
+--Number of Customer by region
+SELECT 
+    Region,
+    COUNT(DISTINCT CustomerID) AS TotalCustomers
+FROM 
+    dbo.customerdata
+GROUP BY 
+    Region;
+
+--The most common type of Subscription type by customer count
+SELECT TOP 1
+    SubscriptionType,
+    COUNT(DISTINCT CustomerID) AS CustomerCount
+FROM 
+    dbo.customerdata
+GROUP BY 
+    SubscriptionType
+ORDER BY 
+    CustomerCount DESC;
+
+--Customer who cancelled their subscription within 6 Months
+SELECT 
+    CustomerID,
+    SubscriptionStart,
+    SubscriptionEnd
+FROM 
+    dbo.customerdata
+WHERE 
+    Canceled = 'TRUE'
+    AND DATEDIFF(month, SubscriptionStart, SubscriptionEnd) <= 6;
+
+--Average subscription duration for all customers
+SELECT 
+    AVG(DATEDIFF(MONTH, SubscriptionStart, SubscriptionEnd)) 
+	AS AverageSubscriptionDurationInDays
+FROM 
+    dbo.customerdata
+
+SELECT 
+    AVG(DATEDIFF(day, SubscriptionStart, SubscriptionEnd)) AS AverageSubscriptionDurationInDays
+FROM 
+    Subscriptions;
+
+--Customer with subscrition loger than 12 months
+SELECT 
+    CustomerID,
+    SubscriptionStart,
+    SubscriptionEnd,
+    DATEDIFF(month, SubscriptionStart, SubscriptionEnd) AS SubscriptionDurationInMonths
+FROM 
+    dbo.customerdata
+WHERE 
+    DATEDIFF(month, SubscriptionStart, SubscriptionEnd) > 12;
+
+--Total revenue by subscription type
+SELECT 
+    SubscriptionType,
+    SUM(Revenue) AS TotalRevenue
+FROM 
+    dbo.customerdata
+GROUP BY 
+    SubscriptionType
+ORDER BY 
+    TotalRevenue DESC;
+
+	--Top 3 regions by subcription cancellation
+SELECT TOP 3
+    Region,
+    COUNT(DISTINCT CustomerID) AS CancellationCount
+FROM 
+    dbo.customerdata
+WHERE 
+    Canceled = 'TRUE'
+GROUP BY 
+    Region
+ORDER BY 
+    CancellationCount DESC;
+
+--Total number of active and canceled subscriptions. 
+SELECT 
+    CASE 
+        WHEN Canceled = 'TRUE' THEN 'Canceled'
+        WHEN Canceled = 'FALSE' THEN 'Active'
+    END AS SubscriptionStatus,
+    COUNT(DISTINCT CustomerId) AS TotalCount
+FROM 
+    dbo.customerdata
+GROUP BY 
+    Canceled;
---
+++ SalesData (1).sql
@@ -0,0 +1,105 @@
+--Overview of the sales table
+SELECT 
+	*
+FROM dbo.['LITA Capstone Dataset salesdata$']
+
+--Total sales by product Category
+SELECT 
+	Product,
+	SUM (Quantity*UnitPrice) AS "Total Sales"
+FROM 
+	dbo.['LITA Capstone Dataset salesdata$']
+GROUP BY
+	Product
+
+	--Number of Sales Transaction by Region
+SELECT
+	Region,
+	COUNT(OrderID) AS "Number of Sales Transaction"
+FROM
+	dbo.['LITA Capstone Dataset salesdata$']
+GROUP BY
+	Region
+
+SELECT
+	Top 1
+	Product,
+	SUM(Quantity*UnitPrice) AS TotalSales
+FROM
+	dbo.['LITA Capstone Dataset salesdata$']
+GROUP BY
+	Product
+ORDER BY 
+    TotalSales DESC
+
+--Total Revenue by Product
+SELECT 
+	Product,
+	SUM (Quantity*UnitPrice) AS "Total Revenue"
+FROM 
+	dbo.['LITA Capstone Dataset salesdata$']
+GROUP BY
+	Product
+
+SELECT 
+    DATENAME(month, OrderDate) AS MonthName,
+    COUNT(*) AS OrderCount
+FROM 
+    dbo.['LITA Capstone Dataset salesdata$']
+GROUP BY 
+    DATENAME(month, OrderDate);
+
+--Monthly Total Sales for the Current Year
+SELECT 
+    DATENAME(month, OrderDate) AS MonthName,
+    MONTH(OrderDate) AS MonthNumber,
+    SUM(Quantity*UnitPrice) AS MonthlyTotal
+FROM 
+    dbo.['LITA Capstone Dataset salesdata$']
+WHERE 
+    YEAR(OrderDate) = 2024
+GROUP BY 
+    DATENAME(month, OrderDate),
+    MONTH(OrderDate)
+ORDER BY 
+    MonthNumber;
+
+--Top 5 customers by total purchase amount
+SELECT TOP 5 
+    "Customer Id",
+    SUM(Quantity*UnitPrice) AS TotalPurchaseAmount
+FROM 
+    dbo.['LITA Capstone Dataset salesdata$']
+GROUP BY 
+    "Customer Id"
+ORDER BY 
+    TotalPurchaseAmount DESC;
+
+-- calculate the percentage of total sales contributed by each region. 
+SELECT 
+    Region,
+    SUM(Quantity*UnitPrice) AS RegionalSales,
+    ROUND((SUM(Quantity*UnitPrice) * 100.0) / (SELECT SUM(Quantity*UnitPrice) 
+FROM dbo.['LITA Capstone Dataset salesdata$']), 2) AS PercentageOfTotalSales
+FROM 
+    dbo.['LITA Capstone Dataset salesdata$']
+GROUP BY 
+    Region
+ORDER BY 
+    PercentageOfTotalSales DESC;
+
+--Products with no sales in the last quarter. 
+-- Calculate the start and end dates for the last quarter
+DECLARE @StartDate DATE = DATEADD(qq, DATEDIFF(qq, 0, GETDATE()) - 1, 0);
+DECLARE @EndDate DATE = DATEADD(qq, DATEDIFF(qq, 0, GETDATE()), 0);
+
+-- Find products with no sales in the last quarter
+SELECT 
+    Product
+FROM 
+    SalesData
+GROUP BY 
+    Product
+HAVING 
+    SUM(CASE WHEN OrderDate >= @StartDate AND OrderDate < @EndDate THEN 1 ELSE 0 END) = 0;
+
